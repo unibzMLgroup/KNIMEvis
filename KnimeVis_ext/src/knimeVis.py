cf1ec065b2039ced918aa877bceac4ad35a24b1a
Thomas Borsani <tborsani@unibz.it>
2024-11-22 11:23:46 +0100

    first commit

---
+++ KnimeVis_ext/src/knimeVis.py
@@ -0,0 +1,14 @@
+import knime.extension as knext
+
+
+main_category = knext.category(
+    path="/community/",
+    level_id="unibz",
+    name="UniBZ",
+    description="Category for Nodes by the unibz KNIME dev team",
+    icon="icons/unibz_icon64.png",
+)
+
+
+#from knimeVisNodes import imageProcessing, imageProcessing2
+from knimeVisNodes import imageLoader
