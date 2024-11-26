d8ce18254ba8b057179ccb7c151b31421e35b4d6
Thomas Borsani <tborsani@unibz.it>
2024-11-26 12:29:20 +0100

    Spatial Filtering working with 3 algorithm. no parameters selection available

--- KnimeVis_ext/src/knimeVis.py
+++ KnimeVis_ext/src/knimeVis.py
@@ -10,5 +10,5 @@ main_category = knext.category(
 )
 
 
-#from knimeVisNodes import imageProcessing, imageProcessing2
-from knimeVisNodes import imageLoader
+#from knimeVisNodes import imageProcessing, imageProcessing2 
+from knimeVisNodes import imageLoader,spatialFiltering
